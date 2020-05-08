1e5b2180d3f55766e1760cb05f6fab189965ce3c
edison <huainian_cl@126.com>
2020-05-08 18:30:15 +0800

    update

--- src/main/java/com/test/TestJVM/classloader/TestJVMLoad21.java
+++ src/main/java/com/test/TestJVM/classloader/TestJVMLoad21.java
@@ -1,7 +1,5 @@
 package com.test.TestJVM.classloader;
 
-import com.sun.crypto.provider.AESKeyGenerator;
-
 import java.lang.reflect.Method;
 
 /**
@@ -16,21 +14,27 @@ import java.lang.reflect.Method;
  *
  **/
 
-public class TestJVMLoad20 {
+public class TestJVMLoad21 {
     public static void main(String[] args) throws Exception {
         TestJVMLoad16 t161 = new TestJVMLoad16("load1");
         TestJVMLoad16 t162 = new TestJVMLoad16("load2");
 
+        //删除classpath的文件
+        t161.setPath("J:\\");
+        t162.setPath("J:\\");
+
         Class clazz1 = t161.loadClass("com.test.TestJVM.classloader.TestAPerson");
         Class clazz2 = t162.loadClass("com.test.TestJVM.classloader.TestAPerson");
 
-        //输出true
+        //输出false
+        //并且调用自定义加载器进行加载
         System.out.println(clazz1 == clazz2);
 
         Object o1 = clazz1.newInstance();
         Object o2 = clazz2.newInstance();
 
         Method method1 = clazz1.getMethod("setTestAPerson",Object.class);
+        //会抛异常,因为是2个不同的类对象
         method1.invoke(o1,o2);
 
     }
