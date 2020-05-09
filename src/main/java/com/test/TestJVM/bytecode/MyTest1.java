4b8144f375728af940d188984a6ec7a24a21fff0
EdisonKon <huainian_cl@126.com>
2020-05-10 00:14:17 +0800

    字节码

---
+++ src/main/java/com/test/TestJVM/bytecode/MyTest1.java
@@ -0,0 +1,20 @@
+package com.test.TestJVM.bytecode;
+
+/**
+ * @Author dekai.kong
+ * @description: 难度
+ * @create: 2020-05-09 23:09
+ * @from:
+ **/
+
+public class MyTest1 {
+    private int a = 1;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
